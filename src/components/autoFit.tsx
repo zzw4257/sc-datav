a672a045fc5fa43cd82f1ea8740c1e51b79fe891
knight <941122922@qq.com>
2025-12-03 21:02:41 +0800

    fix: 自适应组件拆分

---
+++ src/components/autoFit.tsx
@@ -0,0 +1,29 @@
+import { useLayoutEffect } from "react";
+import styled from "styled-components";
+import autofit from "autofit.js";
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  z-index: 100;
+  display: flex;
+  flex-direction: column;
+`;
+
+export default function AutoFit(
+  props: Omit<typeof Wrapper.defaultProps, "id">
+) {
+  const id = `autofit_${Date.now().toString(36)}`;
+
+  useLayoutEffect(() => {
+    autofit.init({ el: `#${id}` });
+
+    return () => {
+      autofit.off();
+    };
+  }, []);
+
+  return <Wrapper id={id} {...props} />;
+}
