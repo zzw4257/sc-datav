3922931131d9f36f4700db349e9bf20447b671e9
knight <941122922@qq.com>
2025-12-28 17:18:37 +0800

    fix: 删除demo2无用参数

--- src/pages/Demo2/stores/index.ts
+++ src/pages/Demo2/stores/index.ts
@@ -2,27 +2,13 @@ import { create } from "zustand";
 import { subscribeWithSelector } from "zustand/middleware";
 
 interface ConfigStore {
-  toggleBloom: boolean;
-  toggleMirror: boolean;
   mapPlayComplete: boolean;
-  cloud: boolean;
-  bar: boolean;
-  rotation: boolean;
-  heat: boolean;
-  mode: boolean;
   toggle: (key: keyof Omit<ConfigStore, "toggle">) => void;
 }
 
 export const useConfigStore = create<ConfigStore>()(
   subscribeWithSelector((set) => ({
-    toggleBloom: false,
-    toggleMirror: true,
     mapPlayComplete: false,
-    cloud: true,
-    bar: true,
-    rotation: true,
-    heat: true,
-    mode: true,
     toggle: (key) => set((s) => ({ [key]: !s[key] })),
   }))
 );
